696999d15e6dc79af83bd731a5767046a3cebac1
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-11-09 17:20:20 +0100

    feat: update to Swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,15 +1,10 @@
-// swift-tools-version: 5.10
+// swift-tools-version:6.0
 
-///
 import PackageDescription
 
-
-///
 let package = Package(
     name: "EmptyCodable-package",
     products: [
-        
-        ///
         .library(
             name: "EmptyCodable-module",
             targets: ["EmptyCodable-module"]
@@ -17,14 +12,10 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        
-        ///
         .target(
             name: "EmptyCodable-module",
             dependencies: []
         ),
-        
-        ///
         .testTarget(
             name: "EmptyCodable-module-tests",
             dependencies: ["EmptyCodable-module"]
